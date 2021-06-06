5d049e8b3326569f31b86c18e4f65d2332f1ece5
shofol <jahananower@gmail.com>
2021-06-06 19:50:57 +0600

    add meta components

---
+++ components/Meta.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Meta = ({ title, keywords, description }) => {
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+                <meta name="description" content={description} />
+                <meta name="keywords" content={keywords} />
+                <meta name="language" content="en" />
+
+                <meta charSet="UTF-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="icon" href="/favicons/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+                <link rel="manifest" href="/favicons/site.webmanifest" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="theme-color" content="#ffffff"></meta>
+
+                <meta property="og:url" content="http://thedefi.network/" />
+                <meta property="og:title" content="Defi Network LLC — We build digital products & apps on the blockchain." />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content="/social.png" />
+                <meta property="og:description" content="Defi Network LLC — We build digital products & apps on the blockchain." />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="http://thedefi.network/" />
+                <meta property="twitter:title" content="Defi Network LLC" />
+                <meta property="twitter:description" content="Defi Network LLC — We build digital products & apps on the blockchain." />
+                <meta property="twitter:image" content="/social.png" />
+
+            </Head>
+        </div>
+    )
+}
+
+Meta.defaultProps = {
+    title: 'Defi Network LLC',
+    keywords: 'blockchain, defi, finance, bitcoin, etherium, web3, ',
+    description: "We’re The Defi Network. We build digital products & apps on the blockchain. The Defi Network is a global blockchain product team."
+}
+
+
+export default Meta
--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/dist/client/router';
 import Link from 'next/link';
 import React, { useState, useEffect } from 'react'
 import { ProjectData, ProjectModel } from '../../data/projectsData';
+import Meta from '../Meta';
 import ProjectGroup from './ProjectGroup';
 
 const Projects = () => {
@@ -38,21 +39,24 @@ const Projects = () => {
 
 
     return (
-        <div id="projects" className="mt-32 lg:max-w-4xl xl:max-w-5xl xl:pl-8 2xl:max-w-7xl lg:mx-auto">
-            <h2 className={"font-bold pl-4 lg:pl-0 " + (isProjectRoute ? 'text-5xl mb-12' : 'text-2xl mb-8')}>{isProjectRoute ? 'All' : 'Past'} Projects</h2>
-            {
-                groupedProjects.map(
-                    (groupProj: ProjectModel[], index: number) => {
-                        return <ProjectGroup key={index} projectGroup={groupProj}></ProjectGroup>
-                    }
-                )
-            }
-            {!isProjectRoute && <div className="mt-32 text-2xl font-medium font-neueMachina pl-4 lg:pl-0">
-                <Link href="/projects"><a>View all projects ↗</a></Link>
-                <div className="bg-gray-100 h-0.5 mt-8"></div>
+        <>
+            <Meta title={'Projects of The Defi Network LLC'} />
+            <div id="projects" className="mt-32 lg:max-w-4xl xl:max-w-5xl xl:pl-8 2xl:max-w-7xl lg:mx-auto">
+                <h2 className={"font-bold pl-4 lg:pl-0 " + (isProjectRoute ? 'text-5xl mb-12' : 'text-2xl mb-8')}>{isProjectRoute ? 'All' : 'Past'} Projects</h2>
+                {
+                    groupedProjects.map(
+                        (groupProj: ProjectModel[], index: number) => {
+                            return <ProjectGroup key={index} projectGroup={groupProj}></ProjectGroup>
+                        }
+                    )
+                }
+                {!isProjectRoute && <div className="mt-32 text-2xl font-medium font-neueMachina pl-4 lg:pl-0">
+                    <Link href="/projects"><a>View all projects ↗</a></Link>
+                    <div className="bg-gray-100 h-0.5 mt-8"></div>
 
-            </div>}
-        </div>
+                </div>}
+            </div>
+        </>
     )
 }
 
